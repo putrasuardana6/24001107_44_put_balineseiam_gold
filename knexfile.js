c18ff201715bc8fed2b70a554b0a5be8728e54b8
Putra Suardana <putrasuardana6@gmail.com>
2024-05-28 00:23:34 +0800

    Express & Knex

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: "pg",
+    connection: {
+      host: "127.0.0.1",
+      user: "postgres",
+      password: "utamon666",
+      database: "postgres",
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
+};
